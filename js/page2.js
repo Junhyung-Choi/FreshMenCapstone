ee45f960dc1c2a2b75bc443f7c9952d99b579dad
HYUeunju <spet365@hanyang.ac.kr>
2023-11-20 20:53:17 +0900

    bar

---
+++ js/page2.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [2, 4, 6, 8, 10, 12],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
