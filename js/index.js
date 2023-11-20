3a9ffbae8a74392a61723d8b5c5c88225da68198
Junhyung-Choi <junhyung9571@naver.com>
2023-11-20 20:04:20 +0900

    css/ js/ 폴더 추가함

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const ctx = document.getElementById('myChart');
+const data = [12, 19, 3, 5, 2, 3];
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        data: data,
+        // label: data,
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
