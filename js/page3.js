05f659c3ee6fa6aff2d9500d302ede4d33ef8228
youngHYU <kdudddus@hanyang.ac.kr>
2023-12-04 20:56:31 +0900

    데이터 받아오고 그래프 추가

--- js/page3.js
+++ js/page3.js
@@ -1,21 +1,64 @@
-const ctx = document.getElementById('myChart');
-const data = [2, 4, 6, 8, 10, 12];
+const ctx = document.getElementById('myChart1');
+const ctx2 = document.getElementById('myChart2');
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-        data: data,
+// const data = [2, 4, 6, 8, 10, 12];
+function draw(da) {
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['1', '2', '3', '4', '5', '6','7','8','9','10'],
+      datasets: [{
+        data: da,
         // label: data,
-      borderWidth: 2
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+        borderWidth: 2
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  }
-});
+  });
+  new Chart(ctx2, {
+    type: 'polarArea',
+    data: {
+      labels:  ['1', '2', '3', '4', '5', '6','7','8','9','10'],
+      datasets: [{
+        data: da,
+        // label: data,
+        borderWidth: 2
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+}
+function request() {
+  fetch('https://draxasapi.run.goorm.site/analysis/', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      "Content-Type": "application/json"
+    },
+    redirect: 'follow'
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      draw(data);
+    });
+}
+request();
+// draw();
